73a31d99966ec32e1cc7ee1512bc102fb4b522c1
Pedro Wanderley <peu-zico>
2019-12-08 20:07:03 -0300

    icons and animations

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import {
   FlatList, 
   TextInput, 
   TouchableOpacity, 
-  StatusBar 
+  StatusBar,
+  Animated,
+  Dimensions 
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from './data/api.json';
 
@@ -14,6 +18,11 @@ export default function App() {
   const [ igrejas, setIgrejas ] = useState('buscando...');
   const [ busca, setBusca ] = useState('');
   const [ result, setResult ] = useState(''); 
+  const [growAnim] = useState(new Animated.Value(0))
+  const [heightAnim] = useState(new Animated.Value(0))
+  const [fadeAnim] = useState(new Animated.Value(0.3))
+
+  const screenHeight = Math.round(Dimensions.get('window').height);
  
   function handleInput(t) {
     setBusca(t); 
@@ -22,15 +31,47 @@ export default function App() {
   function handleSubmit() {
     var filtro = api.igrejas.filter(
       (i)=> {
-        return i.cidade.toLowerCase().indexOf(busca.toLowerCase()) !== -1 || i.nome.toLowerCase().indexOf(busca.toLowerCase()) !== -1 ;
+        return i.cidade.toLowerCase().trim().indexOf(busca.toLowerCase().trim()) !== -1 
+        || i.nome.toLowerCase().trim().indexOf(busca.toLowerCase().trim()) !== -1 ;
       }
     );
+
     setResult(filtro);
     setBusca('');      
   }
+
+  useEffect(() => {
+    Animated.sequence([
+
+      // Animated.timing(
+      //   heightAnim,
+      //   {
+      //     toValue: screenHeight,
+      //     duration: 1099,
+      //   }
+      // ),
+
+      Animated.timing(
+        fadeAnim,
+        {
+          toValue: 1,
+          duration: 1099,
+        }
+      ),
+      
+      Animated.timing(
+        growAnim,
+        {
+          toValue: 30,
+          duration: 1099,
+        }
+      )
+      
+    ]).start();
+  }, [])
  
   return (
-    <View style={{padding: 20, flex: 1, backgroundColor: "#0336FF"}}>
+    <Animated.View style={{padding: 20, flex: 1 , backgroundColor: "#0336FF", opacity: fadeAnim}}>
       <StatusBar backgroundColor="#1111AA" />
       <TextInput 
         value={busca}
@@ -38,6 +79,7 @@ export default function App() {
         onChangeText={(t)=> handleInput(t)} 
         style={{borderRadius: 4, marginBottom: 20, padding:10, backgroundColor: "white"}} 
       />
+      <Icon name="search" size={20} color="#9999" style={{position: 'absolute', marginTop: '11%', marginLeft: '95%'}} />
 
       {busca.length > 1 && 
         <TouchableOpacity 
@@ -60,21 +102,30 @@ export default function App() {
         keyExtractor={item => item.id}
         renderItem={({item}) => (
           <View style={{borderRadius: 4, padding: 10, margin: 5, backgroundColor: "white"}}>
+
             <Text style={{fontSize: 20, marginBottom: 2, fontWeight: "bold"}}> {item.nome} </Text>
-            <Text style={{color: "#0336FA", marginBottom: 7}}> {(item.denominacao).toUpperCase()} </Text>
-            <Text style={{marginTop: 3, color: "#0003FF", fontWeight: "bold"}}> {item.qnt} membros </Text>
+
+            <Text style={{color: "#0336FA", marginBottom: 7}}> {(item.denominacao)} </Text>
+
+            <View style={{flexDirection: 'row', alignItems: "center"}}>
+              <Icon name="person" size={20} color="#333" />
+              <Text style={{marginTop: 3, color: "#0003FF", fontWeight: "bold", flexDirection: "column", marginLeft: 5}}>
+                {item.qnt} membros 
+              </Text>
+            </View>
+
             <Text style={{marginTop: 3, fontWeight: "bold"}}> Início às {item.horario} </Text>
+
             <Text style={{marginTop: 10, color: "#0336FA"}}> {item.site} </Text>
           </View>  
         )}
       />
-      
         
         {result === '' &&
           <View style={{flex: 1, marginTop: '30%', alignItems: "center"}}>
-            <Text style={{color: "white", fontWeight: "bold", fontSize: 30}}> God's House </Text>
+            <Animated.Text style={{color: "white", fontWeight: "bold", fontSize: growAnim}}> God's House </Animated.Text>
           </View>
         }
-    </View>
+    </Animated.View>
   );
 }
